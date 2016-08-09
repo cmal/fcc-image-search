3fdaf1df92958024e877ca45425e7335c3acb7bf
Yu Zhao <zyzy5730@163.com>
2016-08-10 03:56:14 +0800

    add db operations

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/api/imagesearch", function(req, res, next) {
             + "&cx=006064609831781604018%3A_1uwtb4z7jw&num=10&searchType=image&key="
             + apiKey
             + "&start="
-            + ( isNaN(start) ? 1 : start+1 );
+            + ( (isNaN(start) || start < 0) ? 1 : start+1 );
   console.log('url:', url);
   var newjson = [];
   var request = https.get(url, function(response) {
